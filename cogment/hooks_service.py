052874d5b04f7f9759991460311c6a4f7800d0d9
François Chabot <francois@ai-r.com>
2021-01-21 10:09:16 -0500

    Fix incorrect hook servicer instantiation

--- cogment/hooks_service.py
+++ cogment/hooks_service.py
@@ -26,7 +26,7 @@ import traceback
 
 class PrehookServicer(TrialHooksServicer):
 
-    def __init__(self, impls, cog_settings):
+    def __init__(self, impls, cog_settings, prometheus_registry):
 
         self.__impls = impls
         self.__cog_settings = cog_settings
